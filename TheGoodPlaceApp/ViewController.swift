a05f00c72da8003f76c58e400418ff83c3b9f60f
Girls Who Code <GWC@cs81-gwc-05.local>
2019-07-10 13:06:58 -0400

    Initial Commit

---
+++ TheGoodPlaceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheGoodPlaceApp
+//
+//  Created by Girls Who Code on 7/10/19.
+//  Copyright © 2019 Girls Who Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
